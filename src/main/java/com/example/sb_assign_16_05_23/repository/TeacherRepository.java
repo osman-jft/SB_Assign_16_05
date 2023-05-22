0283607540c7074e67c3c4c9a4870d6ec81a509c
Osman Rabbani <osman@jellyfishtechnologies.com>
2023-05-22 13:02:43 +0530

    implemented tickets 3.1 & 3.2 as requested

---
+++ src/main/java/com/example/sb_assign_16_05_23/repository/TeacherRepository.java
@@ -0,0 +1,10 @@
+package com.example.sb_assign_16_05_23.repository;
+
+import com.example.sb_assign_16_05_23.entity.Student;
+import com.example.sb_assign_16_05_23.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
