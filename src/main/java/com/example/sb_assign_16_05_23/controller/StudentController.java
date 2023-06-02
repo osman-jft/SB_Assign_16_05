566d0ebc649026bd2e244d9ea982526a6333e0d3
gladwin-jft <128125104+gladwin-jft@users.noreply.github.com>
2023-06-02 15:09:14 +0530

    Update StudentController.java

--- src/main/java/com/example/sb_assign_16_05_23/controller/StudentController.java
+++ src/main/java/com/example/sb_assign_16_05_23/controller/StudentController.java
@@ -1,16 +1,18 @@
 package com.example.sb_assign_16_05_23.controller;
 
-import com.example.sb_assign_16_05_23.dto.ResponseDTO;
-import com.example.sb_assign_16_05_23.dto.StudentDTO;
 import com.example.sb_assign_16_05_23.dto.ValidList;
+import com.example.sb_assign_16_05_23.dto.StudentDTO;
+import com.example.sb_assign_16_05_23.dto.ResponseDTO;
 import com.example.sb_assign_16_05_23.service.StudentService;
-import com.example.sb_assign_16_05_23.util.Constants;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.example.sb_assign_16_05_23.util.Constants;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -29,19 +31,18 @@ public class StudentController {
     private ResponseDTO<List<StudentDTO>> getStudents() {
         List<StudentDTO> studentDto = studentService.getAllStudents();
         if (studentDto == null) { // check null condition for list
-            return ResponseDTO.<List<StudentDTO>>builder().data(null).message(Constants.EMPTY_LIST).
-                    status(HttpStatus.NO_CONTENT.value())
+            return ResponseDTO.<List<StudentDTO>>builder()
+                .data(null).message(Constants.EMPTY_LIST).status(HttpStatus.NO_CONTENT.value())
                     .build();
         }
 
-        return ResponseDTO.<List<StudentDTO>>builder().data(studentDto).message(Constants.SUCCESS_MSG).
-                status(HttpStatus.OK.value())
+        return ResponseDTO.<List<StudentDTO>>builder()
+            .data(studentDto).message(Constants.SUCCESS_MSG).status(HttpStatus.OK.value())
                 .build();
     }
-
     @PostMapping("/list")
     public ResponseDTO<List<StudentDTO>> registerStudentsList(@RequestBody @Valid ValidList<StudentDTO> studentDtos) {
-        List<StudentDTO> dtos = studentService.registerStudentList(studentDtos);
+        List<StudentDTO> dtos =studentService.registerStudentList(studentDtos);
 
         return ResponseDTO.<List<StudentDTO>>builder()
                 .data(dtos).message(Constants.CREATED).status(HttpStatus.CREATED.value())
