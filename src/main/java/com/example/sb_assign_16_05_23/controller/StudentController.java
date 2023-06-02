b7a0cc713920ae4c139abc8a2afeb6db00ef5be0
gladwin-jft <128125104+gladwin-jft@users.noreply.github.com>
2023-06-02 15:14:13 +0530

    Update StudentController.java

--- src/main/java/com/example/sb_assign_16_05_23/controller/StudentController.java
+++ src/main/java/com/example/sb_assign_16_05_23/controller/StudentController.java
@@ -26,23 +26,23 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-
     @GetMapping
     private ResponseDTO<List<StudentDTO>> getStudents() {
         List<StudentDTO> studentDto = studentService.getAllStudents();
         if (studentDto == null) { // check null condition for list
             return ResponseDTO.<List<StudentDTO>>builder()
-                .data(null).message(Constants.EMPTY_LIST).status(HttpStatus.NO_CONTENT.value())
+                     .data(null).message(Constants.EMPTY_LIST).status(HttpStatus.NO_CONTENT.value())
                     .build();
         }
 
         return ResponseDTO.<List<StudentDTO>>builder()
-            .data(studentDto).message(Constants.SUCCESS_MSG).status(HttpStatus.OK.value())
+                .data(studentDto).message(Constants.SUCCESS_MSG).status(HttpStatus.OK.value())
                 .build();
     }
+    
     @PostMapping("/list")
     public ResponseDTO<List<StudentDTO>> registerStudentsList(@RequestBody @Valid ValidList<StudentDTO> studentDtos) {
-        List<StudentDTO> dtos =studentService.registerStudentList(studentDtos);
+        List<StudentDTO> dtos = studentService.registerStudentList(studentDtos);
 
         return ResponseDTO.<List<StudentDTO>>builder()
                 .data(dtos).message(Constants.CREATED).status(HttpStatus.CREATED.value())
