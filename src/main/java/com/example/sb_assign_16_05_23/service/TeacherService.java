92b48fd19d84e26f31c61161baec69938025a090
gladwin-jft <128125104+gladwin-jft@users.noreply.github.com>
2023-06-02 14:57:00 +0530

    Update TeacherService.java

--- src/main/java/com/example/sb_assign_16_05_23/service/TeacherService.java
+++ src/main/java/com/example/sb_assign_16_05_23/service/TeacherService.java
@@ -1,18 +1,14 @@
 package com.example.sb_assign_16_05_23.service;
 
-
 import com.example.sb_assign_16_05_23.dto.TeacherDTO;
+import com.example.sb_assign_16_05_23.entity.Teacher;
 
 import java.util.List;
 
 public interface TeacherService {
 
-
     List<TeacherDTO> getAllTeachers();
-
     List<TeacherDTO> setTeachers(TeacherDTO teacherData);
-
     List<TeacherDTO> setAll(List<TeacherDTO> teacherData);
-
 }
 
