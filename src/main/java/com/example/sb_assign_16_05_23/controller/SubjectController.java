7b4aa5f3a982f1d53c1e1d0f4b3ce3b5cf6452a0
Gladwin <gladwin@jellyfishtechnologies.com>
2023-06-09 11:35:18 +0530

    SubjectController conflict resolved

--- src/main/java/com/example/sb_assign_16_05_23/controller/SubjectController.java
+++ src/main/java/com/example/sb_assign_16_05_23/controller/SubjectController.java
@@ -5,7 +5,11 @@ import com.example.sb_assign_16_05_23.dto.SubjectDTO;
 import com.example.sb_assign_16_05_23.service.SubjectService;
 import com.example.sb_assign_16_05_23.util.Constants;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
